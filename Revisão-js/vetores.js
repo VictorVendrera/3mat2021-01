2c9a3c5f16c739f35f2e429a89436a01f09b6aa3
VictorVendrera <victorvendreravieira@gmail.com>
2021-02-15 23:25:03 +0000

    (15/02)Finalização da revisão de vetores

--- Revisão-js/vetores.js
+++ Revisão-js/vetores.js
@@ -50,3 +50,46 @@ console.log(frutas)
 //Substituindo 6º fruta
 frutas.splice(5,1, 'abacate')
 console.log(frutas)
+
+/************************************************* 
+ * Percursos de Vetor
+*/
+
+// 1) Percuso via for tradicional
+//É o mais flexível, permitindo o percurso parcial (pode começar
+//em um elemento que não seja o primeiro e terminar em um elemento
+//que não seja o último)
+for(let i = 0; i<frutas.length;i++){
+    console.log(i, frutas[i])
+}
+
+console.log('=========================================')
+
+//2) Percurso via for tradicional, em ordem inversa
+for(let i = frutas.length - 1;i>=0;i--){
+    console.log(i, frutas[i])
+}
+
+//3)Percurso usando for..of
+//Percorre totalmente um vetor, sem necessidade manter uma variável contadora
+//Varáveis:
+//Fruta => nome da varável que receberá cada elemento(pode ser qqr nome válido
+//de variável)
+//frutas => o nome do vetor que está sendo percorrido
+for(let fruta of frutas){
+    console.log(fruta)
+}
+
+//4) Percurso utilizando forEach()
+//forEach() recebe como argumento uma função cujo argumento
+//é cada um dos elementos do vetor percorrido
+//Podia ser usado qlq nome no lugar de elemento
+frutas.forEach(function(elemento){
+    console.log(elemento)
+
+})
+
+console.log("================================")
+
+//forEach() utilizando arrow function como parâmetro
+frutas.forEach(elemento => console.log(elemento))
